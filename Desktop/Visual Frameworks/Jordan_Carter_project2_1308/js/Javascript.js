2dfdd91025dd202c0921fdfd5d67ab8ce23637cc
Jordan Carter <jcarter2fullsail.edu>
2013-08-14 14:24:05 -0400

    had local storage working and the drop down box working now neither is

--- Desktop/Visual Frameworks/Jordan_Carter_project2_1308/js/Javascript.js
+++ Desktop/Visual Frameworks/Jordan_Carter_project2_1308/js/Javascript.js
@@ -26,20 +26,68 @@ alert("javascript loaded!!");
             makeOption.innerHTML = optText;
             makeSelect.appendChild(makeOption);
         }
-        selectLi.appendChild(makeSelect);
+        selectLi.appendChild(makeSelect)
     }
     
+    
+   
+    
+    //find the value of selected radio button
+    function getSelectedRadio() {
+        var radio = document.forms[0].recurring;
+        for (var i=0; i<radio.length;i++) {
+            if (radio[i].checked) {
+                recurringValue=radio[i].value;
+            }
+            
+        }
+    }
+    
+    //find checkbox value    
+    function getCheckboxValue() {
+        if ($('remember').checked) {
+            rememberValue= $('remember').value;
+        }else{
+            rememberValue= "no";
+        }
+    }
+    
+    function storeFormInfo() {
+         var id          	= Math.floor(Math.random()*100000000);
+        getSelectedRadio();
+        getCheckboxValue();
+        //gather up our values in an object.
+        //object properties contain an array w/ form lable and inputs values.
+        var item            = {};
+            item.ItWasA     = ["Dream Kind: ",$('dreamType').value];
+            item.specifics  = ["Any Specifics: ", $('specifics').value];
+            item.important  = ["Level of Importance: ",$('importance').value];
+            item.privileged = ["How Privileged Is This Info?",recurringValue];
+            item.remember   = ["I Remember?", rememberValue];
+            item.indepth    = ["Tell Me More: ", $('indepth').value];
+        //save data to local storage. Use stringafy to convert our objects to a string
+        localStorage.setItem(id, JSON.stringify(item))
+        alert("ok, got it!");
+        
+    }
+    
+    
+   
+    
     //variables
-    var dreamKind =["--what kind of dream?--", "dream","nightmare","visonary"];
+    var dreamKind =["--what kind of dream?--", "dream", "nightmare", "visonary"]
+       recurringValue,
+       rememberValue;
     makeSelect();
+    
     /*click eventListeners
     var displayLink =$("displayData")
-    displayLink.addEventListener("click", getData);
-    var clearLink = $("clear");
+    displayLink.addEventListener("click", displayData);
+    /*var clearLink = $("clear");
     clearLink.addEventListener("click", clearData);
-    var save = $("submit");
-    save.addEventListener("click", storeData);
-    document.addEventListener("click", saveInfo)*/
+    */var save = $("submit");
+    save.addEventListener("click", storeFormInfo);
+    
 
 
 });
